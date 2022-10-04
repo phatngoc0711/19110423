b1fc0e92262021ee456441ba63e5a1eb4ce1fc37
phatngoc0711 <phat07112001@gmail.com>
2022-10-05 00:57:26 +0700

    first commit

---
+++ models/models.posts.js
@@ -0,0 +1,25 @@
+var posts = [{
+    id: 0,
+    title: 'New Technology',
+    content: `Build Full Stack Application from Scratch with MongoDB, Express, React and NodeJS`,
+    comments: [{
+        user: 'phat',
+        comment: 'positive'
+    }, {
+        user: 'tin',
+        comment: 'supeb'
+    }]
+}, {
+    id: 1,
+    title: 'Cloud',
+    content: 'Amazon Web Services offers reliable, scalable, and inexpensive cloud computing services. Free to join, pay only for what you use',
+    comments: [{
+        user: 'phuoc',
+        comment: 'perfect'
+    }, {
+        user: 'phat',
+        comment: 'oh well br'
+    }]
+}]
+
+module.exports = posts
